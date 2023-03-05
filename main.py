2ada07d7f07c133b78fa85aad540b1cb081046d8
Isabelle (Loariya-Fiaba) Zennah <113255772+xiacodes@users.noreply.github.com>
2023-03-05 18:16:10 +0000

    Added the message, hashtag and date

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ import os
 # 2 - Add important infomation
 client = os.environ['client']
 
+# 3 - Set the message and hashtag
+message = "Hello Tumblr"
+hashtag = "#HelloTumblr"
+
+# 4 - Set the post date and time
+now = datetime.datetime.now()
+post_date = now + datetime.timedelta(days=1)
+post_time = datetime.time(hour=12, minute=0)
